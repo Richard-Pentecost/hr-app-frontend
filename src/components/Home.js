0c3db9dc26f4951503d4ae1100dc1e974d548ba2
Richard Pentecost <richard.pentecost@madetech.com>
2020-11-02 14:10:16 +0000

    Home page rendering user information

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,37 @@ import { users } from '../data.json';
 
 const Home = props => {
     const [firstName, setFirstName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [role, setRole] = useState('');
+    const [email, setEmail] = useState('');
+    const [telephone, setTelephone] = useState('');
 
     useEffect(() => {
       const user = users.find(item => item.id === props.id);
       if (user) {
         setFirstName(user.firstName);
+        setSurname(user.surname);
+        setRole(user.role);
+        setEmail(user.email);
+        setTelephone(user.telephone);
       }
     }, [props.id]);
 
+    let userInfo = null;
+    if (firstName && role && email && telephone) {
+      userInfo = ( 
+        <>
+          <h1>Welcome {firstName}!</h1>
+          <h1>{role}</h1>
+          <h1>{email}</h1>
+          <h1>{telephone}</h1>
+        </>
+      );
+    }
+
     return (
       <div>
-        <h1>Homepage</h1>
-        { firstName ? <h1>Hello {firstName}</h1> : null}
+        {userInfo}
       </div>
     )
 }
