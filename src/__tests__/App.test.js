1e1c0ed46606affdb5d5e90cd94ad8c947b6b155
joannafawl <joanna.fawl@madetech.com>
2020-11-02 17:11:49 +0000

    Added more tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,12 +2,26 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import App from '../components/App';
+import toJson from "enzyme-to-json";
 
 describe('App', () => {
   it('renders a div with the className = "app"', () => {
-    const wrapper = Enzyme.shallow((
-      <App />
-    ));
+    const wrapper = Enzyme.shallow(<App />);
     expect(wrapper.find('div').hasClass('app')).toBe(true)
   });
+
+  it("renders without crashing", () => {
+    Enzyme.shallow(<App />);
+  });
+
+  it.skip("can skip a test", () => {
+    Enzyme.shallow(<App />);
+  });
+
+  it("renders correctly", () => {
+    const tree = Enzyme.shallow(<App />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
+
 })
+// press u to update screenshot
---
+++ src/__tests__/Login.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import Login from '../components/Login';
+
+describe('Login', () => {
+    it("renders Login Page heading", () => {
+        const wrapper = Enzyme.shallow((<Login />));
+        const header = <h1 className='text'>Login Page</h1>;
+        expect(wrapper.contains(header)).toEqual(true);
+      });
+});
+
+// ****** Mocks and spies ******
+
+// const user = {
+//     name: "Adeneye David",
+//     email: "david@gmail.com",
+//     username: "Dave",
+//   };
+
+// describe("Login", () => {
+//     it("accepts user account props", () => {
+//       const wrapper = mount(<Account user={user} />);
+//       expect(wrapper.props().user).toEqual(user);
+//     });
+
+//     it("contains users account email", () => {
+//       const wrapper = mount(<Account user={user} />);
+//       const value = wrapper.find("p").text();
+//       expect(value).toEqual("david@gmail.com");
+//     });
+//   });
