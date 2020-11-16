1e9b4ec9e8481be3e1da3403c1569a0cbcb32c70
Richard Pentecost <richard.pentecost@madetech.com>
2020-11-16 09:03:15 +0000

    token manager added

---
+++ src/utils/token-manager.js
@@ -0,0 +1,24 @@
+import decode from 'jwt-decode';
+import moment from 'moment';
+
+const setToken = token => {
+  window.localStorage.setItem('apiToken', token);
+};
+
+const getToken = () => window.localStorage.getItem('apiToken');
+
+const getTokenPayload = () => {
+  const token = getToken();
+  return token && decode(token);
+};
+
+const isTokenValid = () => {
+  const token = getTokenPayload();
+  return Boolean(token && ((!token.exp) || (moment().unix() < token.exp)));
+};
+
+const removeToken = () => {
+  window.localStorage.removeItem('apiToken');
+};
+
+export default { setToken, getToken, getTokenPayload, isTokenValid, removeToken };
