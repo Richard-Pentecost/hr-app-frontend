7a619b8d288dcd1de9a0a523db0373826caafae3
nax2uk <azlinayeo@gmail.com>
2020-11-17 14:20:36 +0000

    added edit employee page

---
+++ src/pages/EditEmployee.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import { URL } from '../utils/Constants';
+import axios from 'axios';
+import BreadcrumbBar from '../components/BreadcrumbBar';
+import Heading from '../components/Heading';
+import Form from '../components/Form';
+import { withRouter } from 'react-router';
+
+import '../style/CreateEmployee.scss';
+
+const EditEmployee = ({history, isLoggedIn, currentEmployeeId}) => {
+    const [currentEmployee, setCurrentEmployee] = useState({});
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+                const response = await axios.get(`${URL}/user/${currentEmployeeId}`);
+                setCurrentEmployee(response.data.user);
+                console.log(currentEmployee)
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchUser();
+    }, [setCurrentEmployee, currentEmployeeId]);
+    
+
+    const handleInputChange = event => {
+        if (event.target === undefined) {
+            setCurrentEmployee({
+                ...currentEmployee,
+                'doB': event,
+            })
+        } else {
+            setCurrentEmployee({
+                ...currentEmployee,
+                [event.target.name]: event.target.value}
+            );
+        }
+        
+    }
+
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.put(`${URL}/user/${currentEmployeeId}`, currentEmployee );
+            console.log(response.data);
+            setCurrentEmployee(response.data.user);
+
+            history.push('/home');
+            
+        } catch (error) {
+            console.log(error.response);
+        }
+    }
+    const { firstName, surname, email , role, location, address, nextOfKin, doB, telephone, permissionLevel, salary } = currentEmployee;
+    const formArr = [
+        { type: 'text', value: firstName, name: 'firstName', label: 'First name' },
+        { type: 'text', value: surname, name: 'surname', label: 'Surname' },
+        { type: 'text', value: role, name: 'role', label: 'Role' },
+        { type: 'email', value: email, name: 'email', label: 'Email' },
+        { type: 'text', value: telephone, name: 'telephone', label: 'Telephone' },
+        { type: 'date', value: doB, name: 'doB', label: 'Date of Birth' },
+        { type: 'select', value: permissionLevel, name: 'permissionLevel', label: 'Permission Level' },  
+        { type: 'text', value: nextOfKin, name: 'nextOfKin', label: 'Next of Kin' },
+        { type: 'text', value: salary, name: 'salary', label: 'Salary' },
+        { type: 'text', value: location, name: 'location', label: 'Location' },
+        { type: 'text', value: address, name: 'address', label: 'Address' },
+
+    ];
+
+    return (
+        <>
+            <BreadcrumbBar page='Edit Employee' prevPage='Employee Information' prevLink='/view-employee'/>
+            <div className='headingContainer'>
+                <Heading>Edit Employee</Heading>
+            </div>
+            <div className='formContainer'>
+                <Form 
+                    formArr={formArr}
+                    handleInputChange={handleInputChange}
+                    handleSubmit={handleSubmit}
+                    btnText='Save'
+                />
+            </div>
+        </>
+    );
+}
+
+export default withRouter(EditEmployee);
