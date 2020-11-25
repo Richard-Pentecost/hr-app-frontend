5441ecd8326b8d7c43cd49a829bd72c38ebcf06a
nax2uk <azlinayeo@gmail.com>
2020-11-25 11:23:57 +0000

    removed setUser warning

--- src/pages/ChangePassword.js
+++ src/pages/ChangePassword.js
@@ -37,7 +37,7 @@ const ChangePassword = ({history, user, setUser}) => {
             }
         };
         fetchUser();
-    }, []);
+    }, [setUser]);
     
 
     const handleInputChange = event => {
