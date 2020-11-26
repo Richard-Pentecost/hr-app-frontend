1c7cd6e6f093b088273bb53cd45b566c5bdfd60d
Richard Pentecost <richard.pentecost@madetech.com>
2020-11-26 09:42:12 +0000

    styling to buttons in tables

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -3,27 +3,32 @@ import Table from '@govuk-react/table';
 import Button from '@govuk-react/button';
     
 const EmployeeTable = ({users, handleClick, deleteHandler, adminLevel}) => {
-
+    const cellStyles = {padding: '10px'};
     return (
         <Table style={{width:'85%', justifyContent: 'center', margin:'40px 50px'}}>
           <Table.Row>
-            <Table.CellHeader>First Name</Table.CellHeader>
-            <Table.CellHeader>Surname</Table.CellHeader>
-            <Table.CellHeader>Email</Table.CellHeader>
-            <Table.CellHeader>Role</Table.CellHeader>
-            <Table.CellHeader>Location</Table.CellHeader>
-            {adminLevel === "Admin" && <Table.CellHeader></Table.CellHeader>}
+            <Table.CellHeader style={cellStyles}>First Name</Table.CellHeader>
+            <Table.CellHeader style={cellStyles}>Surname</Table.CellHeader>
+            <Table.CellHeader style={cellStyles}>Email</Table.CellHeader>
+            <Table.CellHeader style={cellStyles}>Role</Table.CellHeader>
+            <Table.CellHeader style={cellStyles}>Location</Table.CellHeader>
+            {adminLevel === "Admin" && <Table.CellHeader style={cellStyles}></Table.CellHeader>}
           </Table.Row>
             {users.map((user, index)=>(
             <Table.Row onClick={()=>handleClick(user.userId)} className='tableRow' key={index}>
-              <Table.Cell>{user.firstName}</Table.Cell>
-              <Table.Cell>{user.surname}</Table.Cell>
-              <Table.Cell>{user.email}</Table.Cell>
-              <Table.Cell>{user.role}</Table.Cell>
-              <Table.Cell>{user.location}</Table.Cell>
+              <Table.Cell style={cellStyles}>{user.firstName}</Table.Cell>
+              <Table.Cell style={cellStyles}>{user.surname}</Table.Cell>
+              <Table.Cell style={cellStyles}>{user.email}</Table.Cell>
+              <Table.Cell style={cellStyles}>{user.role}</Table.Cell>
+              <Table.Cell style={cellStyles}>{user.location}</Table.Cell>
               { adminLevel === "Admin" && (
-                <Table.Cell>
-                  <Button buttonColour='#357ebd' buttonHoverColour='#78aace' onClick={(e)=>deleteHandler(e, user.userId)}>Delete</Button>
+                <Table.Cell style={cellStyles}>
+                  <Button 
+                    buttonColour='#357ebd'
+                    buttonHoverColour='#78aace'
+                    onClick={(e)=>deleteHandler(e, user.userId)}
+                    style={{ marginBottom: '0px' }}
+                  >Delete</Button>
                 </Table.Cell>
               )}
           </Table.Row>))}
