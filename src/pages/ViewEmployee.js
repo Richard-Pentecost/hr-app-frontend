f43483f2af1a22787841d6b0dde0da2bb4d862d5
duncanc19 <duncancarter93@gmail.com>
2020-11-27 14:45:26 +0000

    Add contactCard to view employee page with styling

--- src/pages/ViewEmployee.js
+++ src/pages/ViewEmployee.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../components/Card';
+import ContactCard from '../components/ContactCard';
 import axios from 'axios';
 import { withRouter } from 'react-router';
 import "../style/Home.scss";
@@ -9,6 +10,8 @@ import BreadcrumbBar from '../components/BreadcrumbBar';
 import Heading from '../components/Heading';
 import LoadingWrapper from '../components/LoadingWrapper';
 import ErrorPage from '../pages/ErrorPage';
+import GridRow from '@govuk-react/grid-row';
+import GridCol from '@govuk-react/grid-col';
 
 const ViewEmployee = ({ match }) => {
 		const [currentEmployee, setCurrentEmployee] = useState('');
@@ -47,7 +50,14 @@ const ViewEmployee = ({ match }) => {
 			</div>
 			<LoadingWrapper loading={loading}>
 				<div className='userInfo'>
-					<Card user={currentEmployee} link={`/view-employee/${employeeId}/edit-employee`} />
+					<GridRow>
+						<GridCol>
+							{currentEmployee && <Card user={currentEmployee} link={`/view-employee/${employeeId}/edit-employee`} />}
+						</GridCol>
+						<GridCol>
+							{currentEmployee && <ContactCard user={currentEmployee} link={`/view-employee/${employeeId}/edit-employee`} />}
+						</GridCol>
+					</GridRow>
 				</div>
 			</LoadingWrapper>
 		</>
